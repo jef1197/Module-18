1f4b639f147e8ede09a64b073abf7c476d94e348
JefO <jef.ogu@gmail.com>
2023-07-10 15:41:52 -0400

    Update thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,8 +5,8 @@ const {
   createThought,
   updateThought,
   deleteThought,
-  // createReaction,
-  // deleteReaction,
+  createReaction,
+  deleteReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
@@ -18,6 +18,11 @@ router.route('/:thoughtId').get(getSingleThought)
 .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-// router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
 
 module.exports = router;
